81e2760bafb15671fcf938668eb633c07549640d
craw-codurance <cameron.raw@codurance.com>
2021-04-07 15:35:05 +0200

    Initial commit

---
+++ dbdemo_project/DatabaseConnection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace dbdemo_project
+{
+    public class DatabaseConnection
+    {
+        public void CreateConnection()
+        { 
+            string connectionString = @" Server=localhost:1433; Database=GETFLIX; User Id=sa; Password=Yggdrasil1989 ";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("");
+            }
+        }
+    }
+
+    public class Video
+    {
+        public int Id;
+        public string Title;
+        public int SeasonNo;
+        public int EpisodeNo;
+        public bool IsTvShow;
+    }
+    
+    public class Audio
+    {
+        public int Id;
+        public string Url;
+    }
+    
+    public class Subtitle
+    {
+        public int Id;
+        public string Url;
+    }
+}
